788980acddef373b93cc5a9243a37d52409e2ddc
brandonladen <brandonladen486@gmail.com>
2024-09-10 21:55:23 +0300

    added a new account app

--- poll_project/urls.py
+++ poll_project/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('polls_app.urls')),
+    path('auth/', include('account.urls')),
 ]
--- polls_app/models.py
+++ polls_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class post(models.Model):
+    post_name = models.CharField(max_length=200)
+    
+    #SELECT * FROM STUDENT WHERE ID=20;
+    #STUDENT.objects.filter(ID=20)
+    
+    
+# POLITICAL_PARTY_TABLE
+# 	PARTY_ID
+# 	PARTY_NAME
+	
+# POST TABLE
+# 	POST_ID
+# 	POST_NAME
+
+# CANDIDATES TABLE
+# 	CAND_ID
+# 	CAND_NAME
+# 	CAND_POST
+# 	CAND_MANIFESTO
+# 	CAND_VOTES
+# 	CONTACT_DETAILS
+
+# VOTES
+# 	VOTE_ID
+# 	CAND
+# 	VOTES
+# 	DATE/TIME
